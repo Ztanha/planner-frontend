e9bfddf92075c21c5b09f2a31be4972a18d87abb
Xara <xaratanha@gmail.com>
2023-01-09 17:35:45 -0800

    Create dialog component and style

---
+++ src/components/dialog/Dialog.js
@@ -0,0 +1,22 @@
+import './dialog.scss'
+import {Modal} from "../modal/Modal.js";
+
+export default function Dialog(props) {
+
+    return (<div id='dialog-component'>
+        <Modal show={ props.show } hide={ props.hide } >
+                 <div className='dialog-container'>
+                     { props.icon || '' }
+                     <div className='dialog-title'>
+                         { props.title || '' }
+                     </div>
+                     <div className='dialog-content'>
+                         { props.children }
+                     </div>
+                     <div className='dialog-buttons'>
+                          { props.buttons || '' }
+                      </div>
+                  </div>
+        </Modal>
+    </div>)
+}
