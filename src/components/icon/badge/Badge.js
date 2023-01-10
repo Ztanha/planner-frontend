b50feea854be2bb2e2b2c91c6b0a4cbd52264c4e
Xara <xaratanha@gmail.com>
2023-01-10 09:41:59 -0800

    Add hover effects for Badge

--- src/components/icon/badge/Badge.js
+++ src/components/icon/badge/Badge.js
@@ -2,13 +2,26 @@ import redirect from "../../../utilities/redirect.js";
 import './badge.scss';
 import {useTheme} from "../../../ThemeContext.js";
 import {formatColor} from "../../../utilities/colors.js";
+import {useState} from "react";
 
 export default function Badge(props) {
 
-    const [theme]=useTheme();
+    const [state,setState] = useState('default');
+    const [theme] = useTheme();
     let classes = props.active ? 'active ' :'';
     classes = classes+'navbar-icon';
-    const bgColor = `rgba(${ formatColor(theme.primary) }, .08)`
+    const styles={
+        default:{
+            backgroundColor:'transparent'
+        },
+        focused:{
+            backgroundColor: `rgba(${ formatColor(theme.primary) }, .08)`,
+            color:theme.onSurfaceVariant
+        },
+        activated:{
+            backgroundColor: `rgba(${ formatColor(theme.primary) }, .12)`,
+        }
+    }
 
     const handleClick=()=>{
         if(typeof props.url !== "undefined") {
@@ -19,11 +32,20 @@ export default function Badge(props) {
     return(<div className={ classes }>
             <div onClick={ handleClick }
                  className={ 'badge-shape-container' }
-                 style={{backgroundColor:bgColor}}
+                 onMouseEnter={()=>setState('focused')}
+                 onMouseLeave={()=>setState('default')}
+                 onMouseDown={()=>setState('activated')}
+                 style={ props.active
+                     ? styles["focused"]
+                     : styles[state]
+                }
             >
                 { props.icon }
             </div>
-            <div className='title-container'>
+            <div className={ state ==='focused'
+                                ? 'title-container display-block'
+                                : 'title-container'
+            }>
                 { props.title }
             </div>
         </div>
