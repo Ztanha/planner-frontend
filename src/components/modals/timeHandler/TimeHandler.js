4e5df440d41f2f15c318604fba5ac2adff95a0f9
Xara <xaratanha@gmail.com>
2023-01-14 12:50:16 -0800

    Create timeHandler

---
+++ src/components/modals/timeHandler/TimeHandler.js
@@ -0,0 +1,76 @@
+import TimePicker from "react-material-time-picker";
+import React, {useEffect, useRef, useState} from "react";
+
+function TimeHandler(props){
+    const [ secondClockDisplay,setSecondClockDisplay ]=useState(false)
+    const [ firstClockDisplay,setFirstClockDisplay ]=useState(true)
+    const tempStartValue = useRef();
+    const tempEndValue = useRef();
+
+
+    function handleSave(){
+
+        props.setFirstClockValue(tempStartValue.current);
+        props.setSecondClockValue(tempEndValue.current)
+        props.setModalShow(false)
+
+    }
+
+    function handleSwitch(){
+        setFirstClockDisplay(!firstClockDisplay);
+        setSecondClockDisplay(!secondClockDisplay);
+    }
+    useEffect(()=>{
+        if(props.modalShow === true){
+            setFirstClockDisplay(true);
+            setSecondClockDisplay(false);
+        }else{
+            setFirstClockDisplay(false);
+            setSecondClockDisplay(false);
+        }
+    },[props.modalShow])
+
+    return(<>
+        { props.modalShow
+            ?
+            <>
+                <TimePicker onChange={ value=>{tempStartValue.current = value} }
+                            show={ firstClockDisplay }
+                            theme={'light'}
+                            defaultValue={ props.firstClockValue }
+                            hide={ ()=>props.setModalShow(false) }
+                            top={'0'}
+                            buttons={[
+                                {
+                                    label:'Cancel',
+                                    onClick:()=>props.setModalShow(false)
+                                },
+                                {
+                                    label:'Next',
+                                    onClick:handleSwitch
+                                }
+                            ]}
+                />
+                <TimePicker onChange={ value=>{tempEndValue.current = value} }
+                            show={ secondClockDisplay }
+                            theme={'light'}
+                            top={'0'}
+                            defaultValue={ props.secondClockValue }
+                            hide={ ()=>props.setModalShow(false) }
+                            buttons={[
+                                {
+                                    label:'Back',
+                                    onClick:handleSwitch
+                                },
+                                {
+                                    label:'Save',
+                                    onClick:handleSave
+                                }
+                            ]}
+                />
+            </>
+            :''
+        }
+    </>)
+}
+export {TimeHandler}
