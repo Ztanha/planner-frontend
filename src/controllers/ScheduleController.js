540139612289bf53ac45fe2cd06828b32e9ed30a
Xara <xaratanha@gmail.com>
2023-01-19 16:58:40 -0800

    Create scheduleController

---
+++ src/controllers/ScheduleController.js
@@ -0,0 +1,72 @@
+import {fetchWithToken} from "../utilities/fetch.js";
+import {SystemMessage} from "../utilities/showMessage.js";
+
+export default class ScheduleController {
+
+    static async delete(scheduleId){
+        let temp = await fetchWithToken('schedules/', {
+            scheduleId: scheduleId,
+            _method : 'DELETE'
+        })
+        if (temp.status === 'success') {
+            return SystemMessage.success('done');
+        } else {
+            return SystemMessage.error(temp.error)
+        }
+    }
+
+    static async get(date1,date2='') {
+
+        let resp;
+
+        if(date1 === ''){
+
+        }
+        if(date2 !== '') {
+            resp = await fetchWithToken('schedules/', {
+                date: date1,
+                date2: date2,
+            })
+        }else{
+            resp = await fetchWithToken('schedules/', {
+                date: date1,
+            })
+        }
+
+        if (resp.status === 'success') {
+            return SystemMessage.sendData(resp.data);
+        } else {
+            return SystemMessage.error(resp.error)
+        }
+    }
+
+    static async markSchedule(scheduleId, done) {
+
+        let temp = await fetchWithToken('schedules/', {
+            scheduleId : scheduleId,
+            done : +done,
+            _method : 'PUT'
+        })
+        if (temp.status === 'success') {
+            return SystemMessage.success(temp.data);
+        } else {
+            return SystemMessage.error(temp.error)
+        }
+    }
+
+
+    static async add(plansIds, date) {
+
+        let req = await fetchWithToken('schedules/', {
+            plansIds:plansIds,
+            date:date,
+            _method: 'PUT'
+        })
+        if (req.status === 'success') {
+            return SystemMessage.success(req.data);
+        } else {
+            return SystemMessage.error(req.error)
+        }
+    }
+
+}
