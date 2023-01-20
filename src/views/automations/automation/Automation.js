861ca4c555aa4bcff13149f778b5affb50c137b3
Xara <xaratanha@gmail.com>
2023-01-20 11:58:17 -0800

    Rename className buttons to main-btns

--- src/views/automations/automation/Automation.js
+++ src/views/automations/automation/Automation.js
@@ -218,7 +218,7 @@ export default function Automation(){
                     />
                 </div>
 
-                <div className='buttons'>
+                <div className='main-btns'>
                     { schedule.current
                         ?
                         <Button type = { 'outline' }
