d9aa408f1ce5c76aa51f47afeb40357b0bd106e2
Xara <xaratanha@gmail.com>
2023-01-27 13:14:51 -0800

    Create utilities

---
+++ src/utilities/utilities.js
@@ -0,0 +1,52 @@
+export default function hardCopy(arr){
+
+    return JSON.parse(JSON.stringify(arr))
+}
+export function codeWeekdays(days){
+
+    let temp='0000000';
+
+    String.prototype.replaceAt = function(index, replacement) {
+        return this.substring(0, index) + replacement +
+            this.substring(index + replacement.length);
+    }
+
+    for(let i=0; i < days.length ;i++){
+        temp= temp.replaceAt(days[i],'1');
+    }
+    return temp;
+}
+export function decodeWeekdays(weekdays){
+
+    const days = ['Mo','Tu','We','Th','Fr','Sa','Su'];
+    let temp=[];
+    weekdays.split('').forEach((x,index)=>{
+        if(x==='1'){
+            temp.push(days[index]);
+        }
+    })
+    return temp.join(',');
+
+}
+export function normalizeDate(number){
+
+    if(number < 10) {
+        return '0'+number;
+    }else{
+        return number;
+    }
+}
+export function capitalizeFirstChar(string){
+
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+// export function getThePathName(){
+//     const url = location.href;
+//     return url.substring(url.lastIndexOf('/'));
+// }
+
+export function timestampToDay(timestamp) {
+
+    const temp = new Date(timestamp);
+    return `${temp.getFullYear()} ${temp.getMonth()} ${temp.getDate()}}`
+}
