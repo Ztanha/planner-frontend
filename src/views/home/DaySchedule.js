74150fbef9abe32cfb6ea9dec120edb4deeb56aa
Xara <xaratanha@gmail.com>
2023-01-29 15:43:47 -0800

    Change dayTimestamp to useState

--- src/views/home/DaySchedule.js
+++ src/views/home/DaySchedule.js
@@ -12,12 +12,12 @@ import { ReactComponent as Calendar } from "../../scss/icons/event_repeat.svg";
 export default function DaySchedule () {
     const todayTimestamp = dayTimestamp.getTimeStamp();
     const { date } = useParams();
-    const dayTimestamp = date ? new Date(date * 1000) : new Date().getTime();
     const dateInp = useRef();
     const fetchRan = useRef(false);
     const [ input,setInput ] = useState('');
     const initialState = { schedules :[] }
     const [ state,dispatch ] = useReducer( reducer,initialState );
+    const [ dayTimestamp,setDayTimestamp ] = useState()
 
     function reducer(state,action) {
         function sortSchedules(arr)
@@ -123,7 +123,7 @@ export default function DaySchedule () {
         <ListItem overline={ 'Date'}
                   leading={ <Calendar /> }
                   divider={ true }
-                  supportingText={ date ? timestampToDay(date) : '' }
+                  supportingText={ timestampToDay(dayTimestamp) }
         />
         <div id='plans-container'>
             {state.schedules?.length >0
