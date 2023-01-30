83601bc4b6b663f70c8aded0becc485de779f6a2
Xara <xaratanha@gmail.com>
2023-01-29 16:05:03 -0800

    Remove className jooj

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -63,7 +63,6 @@ export default function Home() {
     return(<motion.div initial={{ width: 0 }}
                        animate={{ width:'100%' }}
                        exit={{ x: window.innerWidth ,transition:{ duration: 0.1}}}
-                       className='jooj'
     ><div className='home-page-background'>
         <div className='first'></div>
         <div className='sec'></div>
