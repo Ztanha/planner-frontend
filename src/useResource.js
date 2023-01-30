d0367a9dbced726ff5d38037a724a8cbb95f62a9
Xara <xaratanha@gmail.com>
2023-01-30 12:02:04 -0800

    Create useResource

---
+++ src/useResource.js
@@ -0,0 +1,16 @@
+import {useEffect, useState} from "react";
+import UserController from "./controllers/UserController.js";
+import {fetchWithToken} from "./utilities/fetch.js";
+
+export const useResource = ({ url, data}) =>{
+    const [resource,setResource] = useState(null);
+    useEffect(()=>{
+        ( async ()=>{
+            const response = await  fetchWithToken(url,data);
+            if(response.status === 'success'){
+                setResource(response.data)
+            }
+        })();
+    },[url,data])
+    return resource;
+}
