fa623cae819eb037158077a0b6291dc9ec02733f
Xara <xaratanha@gmail.com>
2023-01-30 15:51:58 -0800

    Ask server if the user with sessionId which is saved in localStorage valid.If so, setUser and redirect to home; Otherwise, show the login form.

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,10 +1,27 @@
 import LoginController from "../../controllers/LoginController.js";
 import {useUser} from "../../UserContext.js";
 import redirect from "../../utilities/redirect.js";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+import {useResource} from "../../useResource.js";
+import {fetchWithToken} from "../../utilities/fetch.js";
 
 export default function Login(){
 
     const [user,setUser] = useUser();
+    const navigate = useNavigate();
+    useEffect(()=>{
+        (async ()=>{
+            const user = await LoginController.checkAuthentication();
+            if( user !== null ) {
+                setUser(user)
+                navigate('/home/')
+            }
+        })();
+    },[])
+
+    //TODO: Ask server if the user with sessionId which is saved in localStorage valid.
+    // TODO: If so, setUser and redirect to home; Otherwise, show the login form.
 
     async function login() {
 
@@ -12,7 +29,7 @@ export default function Login(){
         const password = document.getElementById('password-inp').value;
         const result =  await LoginController.login(uname,password);
         setUser(result);
-        redirect('/home/')
+        navigate('/home/')
 
     }
     console.log(user);
